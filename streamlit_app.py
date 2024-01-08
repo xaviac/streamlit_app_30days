7b572eb6007e58cbf8c41ccb6b6323a43433d2a0
Javier Acevedo <jacevedomx@gmail.com>
2024-01-07 23:44:19 -0600

    Add custom theme and slider widget to Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-st.header('st.latex')
+st.title('st.secrets')
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+st.write(st.secrets['message'])
